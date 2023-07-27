acda7670e2efc4734637cb65d8e61c8908a3fab6
joe <joe.hoole@gmail.com>
2023-07-27 14:55:08 +0100

    coming soon msg

--- src/site.config.js
+++ src/site.config.js
@@ -1,6 +1,6 @@
 export const config = {
 	// Site Title
-	title: 'Svelte Portfolio',
+	title: 'Joe Hoole',
 	// Site Description (max 150 characters)
 	description: 'A Svelte Portfolio Template',
 	// Site URL
