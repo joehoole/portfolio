c315c84c5505a67b0ab9c1f8251c32861b2787f4
joe <joe.hoole@gmail.com>
2024-04-03 22:15:27 +0100

    03_04.4

--- src/site.config.js
+++ src/site.config.js
@@ -2,7 +2,7 @@ export const config = {
 	// Site Title
 	title: 'JOE HOOLE',
 	// Site Description (max 150 characters)
-	description: 'A Svelte Portfolio Template',
+	description: 'Joe Hoole', //SEO THIS LIKE HELL
 	// Site URL
 	url: 'joehoole.co.uk'
 };
