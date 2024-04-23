2b985d17c0ca14b038de6a0774a2e7af173b3fac
joe <joe.hoole@gmail.com>
2024-04-23 13:04:11 +0100

    23_04

--- src/posts/A1_SimulatedSolidarity.md
+++ src/posts/A1_SimulatedSolidarity.md
@@ -5,6 +5,9 @@ slug: simulated-solidarity
 type: Project
 ---
 
+# simulated solidarity
+
+Project initiated as part of masters in research into creative computing at the university of the arts london's creative computing campus in camberwell. the was displayed in Euston in London, as part of a group show with the rest of the [Autonomous Sheep](https://www.autonomous-sheep.com/) collective.
 
 <div class="lists">
 <img src="./images/SimulatedSolidarity/IMG_2886.gif" alt="GIF of artwork with artist Joe Hoole in the foreground with projection above" />
@@ -21,9 +24,12 @@ type: Project
     }
 </style>
 
-# simulated solidarity
+In April 2023, British technology news outlet, The Register, disclosed that the American intelligence technology corporation, Palantir, was under consideration to assist the government to break ongoing National Health Service strikes. However, the UK Government subsequently decided against employing Palantir's services, alluding prohibitive costs as the driver for this decision. Palantir's Foundry platform had been proposed for integration and had raised concerns regarding the handling of sensitive data. Notably, a project designated for "Strike Analysis" was explicitly mentioned in an official Parliamentary communique.
+<br>
+This project looks at this particular idea of simulation and how its used against communities, specifically workers as a stand in for management.
 
-Project initiated as part of masters in research into creative computing at the university of the arts london's creative computing campus in camberwell. the was displayed in Euston in London, as part of a group show with the rest of the [Autonomous Sheep](https://www.autonomous-sheep.com/) collective.
+![An image of the art piece at night from outside the gallery space.](./images/SimulatedSolidarity/IMG_7670.jpg)
 
-![](./images/SimulatedSolidarity/IMG_7670.jpg)
-
+The presented project is a tool for workers to simulate the success of a unionisation ballot whilst being able to control a simulation. With it workers can derive where to best situate themselves to have conversations with their colleagues.
+<br>
+The project subverts the use of simulative tools and make something which proposes a fairer alternative.
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -6,7 +6,45 @@
 
 <div transition:fly={{ y: 100, duration: 500 }}>
 
-<h1>About</h1>
+	<h1>
+		Skills
+	</h1>
+	<div class="lists">
+				
+		<h3>
+			Public speaking
+		</h3>
+		<h3>
+			Game Development
+		</h3>
+		<h3>
+			Research
+		</h3>
+		<h3>
+			Mixed Reality
+		</h3>
+		<h3>
+			Lecturing
+		</h3>
+		<h3>
+			3d Modelling
+		</h3>
+		<h3>
+			TouchDesigner
+		</h3>
+		<h3>
+			Motion Graphics
+		</h3>
+		</div>
+		
+		<style>
+			.lists {display: grid;
+			grid-template-columns: 2fr 2fr
+			
+			}
+		</style>
+
+	<h1>About</h1>
 	<br>
 
 
@@ -15,9 +53,9 @@
 	His research based art project, <a href="simulated-solidarity">Simulated Solidarity</a>, best exemplifies this crossover, which was completed during a Master's in Research at the Creative Computing Institute in University of the Arts London.
 <br> <br>
 	An ardent believer in the freedom of knowledge, Joe teaches digital tools to students aged 6 to 60 years old. Joe has taught on the Design Museum's digital design programme since 2017 and has taken ability to many heights, being part of red carpet events for the London Film Festival, and teaching online classes to schools as far reaching as Beijing University
-	 during the lockdown. He also recently began teaching on the Fine Arts: Computational Arts Bachelors programme at Camberwell College of Arts, part of the University of the Arts London. He is the founder of a Community Interest Corporation, Tinker Studios, that keeps to this commitment of teaching tech to all. 
+	 during the lockdown. He also recently began teaching on the Fine Arts: Computational Arts Bachelors programme at Camberwell College of Arts, part of the University of the Arts London. He is the founder of a Community Interest Corporation, <a href="https://www.tinkerstudios.co.uk">Tinker Studios</a>, that keeps to this commitment of teaching tech to all. 
 	<br> <br>
-	The convergence of design, education, and ethical decision making forms Joe's skillset, on top of all that he has a professional ability at 3d Modelling, and front end Web Development. He is a hobbyist games designer with an interest in all things XR and has a full working knowledge of the Adobe Suite. </p>
+	The convergence of design, education, and ethical decision making forms Joe's skillset, on top of all that he has a professional ability at 3d Modelling, and front end Web Development. He is a games designer with an interest in the world of XR and has a full working knowledge of the Adobe Suite. </p>
 <p>
 	Any questions please email <a href="mailto:joe.hoole@gmail.com"> joe.hoole@gmail.com</a>
 </p>
